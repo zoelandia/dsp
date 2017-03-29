cbec2e41b8a0a066d12e869af391436cbc833c21
Zoe Jacobson <sir.zoe@gmail.com>
2017-03-28 21:29:14 -0400

    calculated days between dates

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,27 @@
 # Hint:  use Google to find python function
 
-####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+import datetime
+import time
 
-####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
+def diff(time1, time2, format):
+    start_conv = datetime.datetime.strptime(time1, format)
+    stop_conv = datetime.datetime.strptime(time2, format)
+    print(stop_conv - start_conv)
 
-####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+####a)
+date_start = '01-02-2013'
+date_stop = '07-28-2015'
+
+diff(date_start, date_stop, '%m-%d-%Y')
+
+####b)
+date_start = '12312013'
+date_stop = '05282015'
+
+diff(date_start, date_stop, '%m%d%Y')
+
+####c)
+date_start = '15-Jan-1994'
+date_stop = '14-Jul-2015'
+
+diff(date_start, date_stop, '%d-%b-%Y')
